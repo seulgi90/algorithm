db429e36f2bfd3b6f343d793537ea7503caf9151
seulgi90 <108708921+seulgi90@users.noreply.github.com>
2022-12-12 00:37:35 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1412 ms, Memory: 130060 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -8,6 +8,9 @@ public class Main {
 
   public static void main(String[] args) throws IOException {
 
+    //    InputStreamReader is = new InputStreamReader(System.in);
+    //    BufferedReader br = new BufferedReader(is);
+
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     // 변수 num에 값을 공백 단위로 구분해서 입력받는다
@@ -18,10 +21,11 @@ public class Main {
     for (int i = 0; i < num; i++) {
       // add 메소드를 사용해서 개행으로 배열 arr[num]에 값을 입력, 리스트에 마지막에 데이터가 추가되게 된다
       arr.add(Integer.parseInt(br.readLine()));
-      //      arr.add(num);
-      //      System.out.println("num:" + arr.add(num));
+      // arr.add(num); // 5 5 5 5 5 5
 
     }
+    // System.out.println("int:" + arr.add(Integer.parseInt(br.readLine())));
+    //  System.out.println("num:" + arr.add(num));
     // 리스트 배열 오름차순 정렬
     Collections.sort(arr);
 
@@ -30,6 +34,7 @@ public class Main {
     for(int value : arr) {
       sb.append(value).append("\n");
     }
+
     System.out.println(sb);
 
 
