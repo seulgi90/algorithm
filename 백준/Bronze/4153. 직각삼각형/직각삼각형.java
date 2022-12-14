705f5729cc3d2ac81fdef4fc0d6e314c5b03c990
seulgi90 <108708921+seulgi90@users.noreply.github.com>
2022-12-14 17:23:50 +0900

    [Bronze III] Title: 직각삼각형, Time: 212 ms, Memory: 17608 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+
+  // 직각삼각형 a*a(대각선 길이) = (b*b) * (c*c)
+
+  public static void main(String[] args) {
+
+    Scanner in = new Scanner(System.in);
+
+    while(true) {
+
+      int x = in.nextInt();
+      int y = in.nextInt();
+      int z = in.nextInt();
+
+      // 0 입력시 종료
+      if(x == 0 && y == 0 && z == 0) break;
+
+
+      if((x * x + y * y) == z * z) {
+        System.out.println("right");
+      }
+      else if(x * x == (y * y + z * z)) {
+        System.out.println("right");
+      }
+      else if(y * y == (z * z + x * x)) {
+        System.out.println("right");
+      }
+      else {
+        System.out.println("wrong");
+      }
+
+    }
+
+
+  }
+}
+
