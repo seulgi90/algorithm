583d7bc0e8d3ff2e0d7437171a41a4c6a1a3b6fe
seulgi90 <108708921+seulgi90@users.noreply.github.com>
2022-12-21 17:17:11 +0900

    [Bronze I] Title: 이항 계수 1, Time: 120 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+
+  public static void main(String[] args) throws IOException {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+    int N = Integer.parseInt(st.nextToken());
+    int K = Integer.parseInt(st.nextToken());
+
+    // nCk = n! / ((n-k)! * k!)
+    System.out.println(factorial(N) / (factorial(N - K) * factorial(K)));
+  }
+
+  static int factorial(int N) {
+    // factorial(0) == 1 , 0!=1 => 0 아님 주의!
+    if (N <= 1) {
+      return 1;
+    }
+    return N * factorial(N - 1);
+  }
+}
